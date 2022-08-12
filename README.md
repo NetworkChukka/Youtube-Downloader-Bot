76412abf5b46d9da3bd965a452bc8ef1b1a626e1
NetworkChukka <89191830+NetworkChukka@users.noreply.github.com>
2022-08-12 08:11:55 +0530

    YT DL BOT

--- README.md
+++ README.md
@@ -14,6 +14,6 @@
     
 ## Thanks ❤️
 * [Spechide](https://telegram.dog/SpEcHIDe) for his [AnyDlBot](https://github.com/SpEcHiDe/AnyDLBot)
-* [HasibulKabir](https://telegram.dog/HasibulKabir)
+* [ME](https://telegram.dog/networkchukka)
 
-[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/aryanvikash/Youtube-Downloader-Bot/tree/master)
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/NetworkChukka/Youtube-Downloader-Bot/tree/master)
--- plugins/start.py
+++ plugins/start.py
@@ -5,10 +5,11 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/aryan_bots")],
+        [InlineKeyboardButton("Channel ❤️", url="https://t.me/anonymousbotz")],
+        [InlineKeyboardButton("Group 🔰", url="https://t.me/anonymousbotzchat")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/aryanvikash")]
+            "Report Bugs 😊", url="https://t.me/networkchukka")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info 🙈"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
